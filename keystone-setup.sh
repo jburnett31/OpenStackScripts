d3cabf6792331767615f41df8036a99799671373
Jeff <jeffrey.burnett31@gmail.com>
2013-05-28 15:41:10 -0500

    reset script

--- keystone-setup.sh
+++ keystone-setup.sh
@@ -13,7 +13,7 @@ MYSQL_HOST=localhost
 MYSQL_PASSWORD=password
 #
 KEYSTONE_REGION=RegionOne
-KEYSTONE_HOST=10.10.10.10
+KEYSTONE_HOST=localhost
 # Shortcut function to get a newly generated ID
 function get_field() {
  while read data; do
@@ -33,11 +33,9 @@ SERVICE_TENANT=$(keystone tenant-create --name=$SERVICE_TENANT_NAME | grep " id 
 ADMIN_USER=$(keystone user-create --name=admin --pass="$ADMIN_PASSWORD" --email=admin@domain.com | grep " id " | get_field 2)
 DEMO_USER=$(keystone user-create --name=demo --pass="$DEMO_PASSWORD" --email=demo@domain.com --tenant-id=$DEMO_TENANT | grep " id " | get_field 2)
 NOVA_USER=$(keystone user-create --name=nova --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=nova@domain.com | grep " id " | get_field 2)
-GLANCE_USER=$(keystone user-create --name=glance --pass="$SERVICE_PASSWORD" \
- --tenant-id $SERVICE_TENANT --email=glance@domain.com | grep " id " | get_field 2)
+GLANCE_USER=$(keystone user-create --name=glance --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=glance@domain.com | grep " id " | get_field 2)
 QUANTUM_USER=$(keystone user-create --name=quantum --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=quantum@domain.com | grep " id " | get_field 2)
-CINDER_USER=$(keystone user-create --name=cinder --pass="$SERVICE_PASSWORD" \
- --tenant-id $SERVICE_TENANT --email=cinder@domain.com | grep " id " | get_field 2)
+CINDER_USER=$(keystone user-create --name=cinder --pass="$SERVICE_PASSWORD" --tenant-id $SERVICE_TENANT --email=cinder@domain.com | grep " id " | get_field 2)
 # Roles
 ADMIN_ROLE=$(keystone role-create --name=admin | grep " id " | get_field 2)
 MEMBER_ROLE=$(keystone role-create --name=Member | grep " id " | get_field 2)
@@ -45,29 +43,20 @@ MEMBER_ROLE=$(keystone role-create --name=Member | grep " id " | get_field 2)
 keystone user-role-add --user-id $ADMIN_USER --role-id $ADMIN_ROLE --tenantid $ADMIN_TENANT
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $NOVA_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $GLANCE_USER --role-id $ADMIN_ROLE
-keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $QUANTUM_USER \
- --role-id $ADMIN_ROLE
+keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $QUANTUM_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $SERVICE_TENANT --user-id $CINDER_USER --role-id $ADMIN_ROLE
 keystone user-role-add --tenant-id $DEMO_TENANT --user-id $DEMO_USER --roleid $MEMBER_ROLE
 # Create services
 COMPUTE_SERVICE=$(keystone service-create --name nova --type compute --description 'OpenStack Compute Service' | grep " id " | get_field 2)
 VOLUME_SERVICE=$(keystone service-create --name cinder --type volume --description 'OpenStack Volume Service' | grep " id " | get_field 2)
 IMAGE_SERVICE=$(keystone service-create --name glance --type image --description 'OpenStack Image Service' | grep " id " | get_field 2)
-IDENTITY_SERVICE=$(keystone service-create --name keystone --type identity \
- --description 'OpenStack Identity' | grep " id " | get_field 2)
-EC2_SERVICE=$(keystone service-create --name ec2 --type ec2 --description \
- 'OpenStack EC2 service' | grep " id " | get_field 2)
+IDENTITY_SERVICE=$(keystone service-create --name keystone --type identity --description 'OpenStack Identity' | grep " id " | get_field 2)
+EC2_SERVICE=$(keystone service-create --name ec2 --type ec2 --description 'OpenStack EC2 service' | grep " id " | get_field 2)
 NETWORK_SERVICE=$(keystone service-create --name quantum --type network --description 'OpenStack Networking service' | grep " id " | get_field 2)
 # Create endpoints
-keystone endpoint-create --region $KEYSTONE_REGION --service-id \
- $COMPUTE_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' --adminurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' \
- --internalurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s'
-keystone endpoint-create --region $KEYSTONE_REGION --service-id $VOLUME_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' --adminurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' \
- --internalurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s'
+keystone endpoint-create --region $KEYSTONE_REGION --service-id $COMPUTE_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' --adminurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s' --internalurl 'http://'"$KEYSTONE_HOST"':8774/v2/$(tenant_id)s'
+keystone endpoint-create --region $KEYSTONE_REGION --service-id $VOLUME_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' --adminurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s' --internalurl 'http://'"$KEYSTONE_HOST"':8776/v1/$(tenant_id)s'
 keystone endpoint-create --region $KEYSTONE_REGION --service-id $IMAGE_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':9292/v2' --adminurl 'http://'"$KEYSTONE_HOST"':9292/v2' --internalurl 'http://'"$KEYSTONE_HOST"':9292/v2'
-keystone endpoint-create --region $KEYSTONE_REGION --service-id
- $IDENTITY_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':5000/v2.0' --adminurl 'http://'"$KEYSTONE_HOST"':35357/v2.0' --internalurl 'http://'"$KEYSTONE_HOST"':5000/v2.0'
-keystone endpoint-create --region $KEYSTONE_REGION --service-id $EC2_SERVICE \
- --publicurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud' --adminurl 'http://'"$KEYSTONE_HOST"':8773/services/Admin' --internalurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud'
-keystone endpoint-create --region $KEYSTONE_REGION --service-id \
- $NETWORK_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':9696/' --adminurl 'http://'"$KEYSTONE_HOST"':9696/' --internalurl 'http://'"$KEYSTONE_HOST"':9696/'
+keystone endpoint-create --region $KEYSTONE_REGION --service-id $IDENTITY_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':5000/v2.0' --adminurl 'http://'"$KEYSTONE_HOST"':35357/v2.0' --internalurl 'http://'"$KEYSTONE_HOST"':5000/v2.0'
+keystone endpoint-create --region $KEYSTONE_REGION --service-id $EC2_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud' --adminurl 'http://'"$KEYSTONE_HOST"':8773/services/Admin' --internalurl 'http://'"$KEYSTONE_HOST"':8773/services/Cloud'
+keystone endpoint-create --region $KEYSTONE_REGION --service-id $NETWORK_SERVICE --publicurl 'http://'"$KEYSTONE_HOST"':9696/' --adminurl 'http://'"$KEYSTONE_HOST"':9696/' --internalurl 'http://'"$KEYSTONE_HOST"':9696/'
---
+++ reset-keystone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mysql -u root -pabc123 -e "drop database keystone;"
+mysql -u root -pabc123 -e "create database keystone;"
+mysql -u root -pabc123 -e "grant all on keystone.* to 'keystone'@'localhost' identified by 'password';"
